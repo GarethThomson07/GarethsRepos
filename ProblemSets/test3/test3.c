0d5ebd7b5f57b9be6957bbb67e4d65baf23bab84
Gareth Thomson <Gthoms04@uoguelph.ca>
2025-10-29 12:28:15 -0400

    idk

---
+++ ProblemSets/test3/test3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i = 0;
+    double N, sum;
+
+    printf("enter number");
+    scanf("%lf", &N);
+    for (i = 0; i < N; i++)
+    {
+        sum += N * N;
+    }
+    printf("%f", sum);
+
+    return 0;
+}
